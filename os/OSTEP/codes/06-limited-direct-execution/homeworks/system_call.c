8cd87a6677e333c88a0e983128dbd26e1462ecc4
sjsakib <sjsakib.bd@gmail.com>
2020-03-21 06:40:59 +0600

    measuring system call time

---
+++ os/OSTEP/codes/06-limited-direct-execution/homeworks/system_call.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <fcntl.h>
+
+
+#define ITERATION_COUNT 10000
+
+int main(int argc, char *argv[]) {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    long tu = t.tv_usec;
+    long ts = t.tv_sec;
+    long sum = 0;
+    long dt;
+    for (int i = 0; i<ITERATION_COUNT; i++) {
+        // do nothing
+    }
+    gettimeofday(&t, NULL);
+    dt = t.tv_usec - tu;
+    assert(ts == t.tv_sec);
+    printf("Average delay for running an empty loop: %.4f total: %ld)\n", dt/(double) ITERATION_COUNT, dt);
+    
+    sum = 0;
+    for (int i = 0; i<ITERATION_COUNT; i++) {
+        gettimeofday(&t, NULL);
+        sum += t.tv_usec - tu;
+        tu = t.tv_usec;
+    }
+    printf("Average delay for getting time: %.4f total: %ld)\n", sum/(double)ITERATION_COUNT, sum);
+    
+    FILE *fp = fopen("XXX", "w");
+    
+    tu = t.tv_usec;
+    ts = t.tv_sec;
+    for (int i = 0; i<ITERATION_COUNT; i++) {
+        fprintf(fp, "");
+    }
+    gettimeofday(&t, NULL);
+    dt = t.tv_usec - tu;
+    assert(ts == t.tv_sec);
+    printf("Average delay for writting empty string: %.4f total: %ld)\n", dt/(double)ITERATION_COUNT, dt);
+
+    tu = t.tv_usec;
+    ts = t.tv_sec;
+    for (int i = 0; i<ITERATION_COUNT; i++) {
+        fprintf(fp, "x");
+    }
+    gettimeofday(&t, NULL);
+    dt = t.tv_usec - tu;
+    assert(ts == t.tv_sec);
+    printf("Average delay for writting single character: %.4f total: %ld)\n", dt/(double)ITERATION_COUNT, dt);
+
+    
+    int fd = open("yyy", O_WRONLY);
+    char *buff;
+    
+    tu = t.tv_usec;
+    ts = t.tv_sec;
+    for (int i = 0; i<ITERATION_COUNT; i++) {
+        write(fd, buff, 0);
+    }
+    gettimeofday(&t, NULL);
+    dt = t.tv_usec - tu;
+    assert(ts == t.tv_sec);
+    printf("Average delay for writting empty string with `write`: %.4f total: %ld)\n",dt/(double)ITERATION_COUNT, dt);
+    return 0;
+}
