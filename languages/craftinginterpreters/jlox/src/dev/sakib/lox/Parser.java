43f2cc1c39d9b9323f055483010c88664ffe4db8
Sakib <sjsakib.bd@gmail.com>
2021-07-14 07:30:52 +0600

    📝 added comma operator

--- languages/craftinginterpreters/jlox/src/dev/sakib/lox/Parser.java
+++ languages/craftinginterpreters/jlox/src/dev/sakib/lox/Parser.java
@@ -24,7 +24,19 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        return comma();
+    }
+
+    private Expr comma() {
+        Expr expr = equality();
+
+        while (match(COMMA)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
     }
 
     private Expr equality() {
