caf36b1bb4d8d088dd6cd98a8fbf3ae8a9fff19a
Sakib <sjsakib.bd@gmail.com>
2021-07-17 18:08:23 +0600

    📝 enable adding string with other types

--- languages/craftinginterpreters/jlox/src/dev/sakib/lox/Interpreter.java
+++ languages/craftinginterpreters/jlox/src/dev/sakib/lox/Interpreter.java
@@ -56,10 +56,10 @@ class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof Double && right instanceof Double) {
                     return (double) left + (double) right;
                 }
-                if (left instanceof String && right instanceof String) {
-                    return (String) left + (String) right;
+                if (left instanceof String || right instanceof String) {
+                    return  stringify(left) + stringify(right);
                 }
-                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or at least one string");
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double) right;
