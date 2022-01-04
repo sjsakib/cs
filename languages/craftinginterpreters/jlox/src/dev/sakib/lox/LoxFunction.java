a49ec05e900626e25e5d0a9e299ec75ea9fcf784
Sakib <sjsakib.bd@gmail.com>
2022-01-05 00:45:51 +0600

    ✔️closure

--- languages/craftinginterpreters/jlox/src/dev/sakib/lox/LoxFunction.java
+++ languages/craftinginterpreters/jlox/src/dev/sakib/lox/LoxFunction.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    public LoxFunction(Stmt.Function declaration) {
+    public LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
@@ -21,7 +23,7 @@ public class LoxFunction implements LoxCallable {
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
